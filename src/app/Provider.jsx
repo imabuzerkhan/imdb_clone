de1dfb73757984bdb6cc8dda442311afe1988b09
imabuzerkhan <abuzerk41@gmail.com>
2024-06-27 10:05:50 +0545

    adding darkmode

---
+++ src/app/Provider.jsx
@@ -0,0 +1,16 @@
+"use client"
+import { ThemeProvider } from 'next-themes'
+const Provider = () => {
+  return (
+   <>
+   <ThemeProvider>
+   <div className='' >
+    {childeren}
+    
+   </div>
+   </ThemeProvider>
+   </>
+  )
+}
+
+export default Provider
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Provider from "./Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Provider defaultTheme ="system" attribute="class" >
         <Header/>
-        {children}</body>
+        {children}
+        </Provider>
+        </body>
  
     </html>
   );
