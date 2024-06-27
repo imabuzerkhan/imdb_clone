2f0a496b580c60f5c95b3f61a09bff926b6e957e
imabuzerkhan <abuzerk41@gmail.com>
2024-06-27 16:13:18 +0545

    loading

---
+++ src/app/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <>
+    <div className="flex justify-center mt-10 ">
+      <img src=" spiner.svg" alt="loading..." />
+    </div>
+    </>
+  )
+}
+
+export default Loading
