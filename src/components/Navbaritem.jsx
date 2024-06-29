f7970848a3e06af978b838689f4b0552d875c031
imabuzerkhan <abuzerk41@gmail.com>
2024-06-29 09:49:56 +0545

    finally compeleted

--- src/components/Navbaritem.jsx
+++ src/components/Navbaritem.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import Link from 'next/link'
-const Navbaritem = ({title , param }) => {
-  return (
-    <>
-    <Link href={`/?genre=${param}`}  >
-    {title}
-    </Link>
-    </>
-  )
-}
+'use client';
 
-export default Navbaritem
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+
+export default function Navbaritem({ title, param }) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get('genre');
+  return (
+    <div>
+      <Link
+        className={`hover:text-amber-600 font-semibold ${
+          genre === param
+            ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+            : ''
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+}
